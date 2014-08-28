cbd20373297370ff1ca406b22245dc34a1b49cf5
Flauen <Flauen@Flauen-NB>
2014-08-28 20:39:07 -0300

    Funcion factorial

---
+++ Functions/FactFunction.php
@@ -0,0 +1,17 @@
+<?php
+///$arg = $_GET[int];
+function fact($arg)
+{
+    echo "aguante java";
+    $factorial=$arg;
+    
+    for ($index = $arg-1; $index > 1; $index--) 
+    {      
+        $factorial = $factorial * $index ;
+        
+    }
+    
+    return $factorial;
+}
+$num = 5;
+echo fact($num);
